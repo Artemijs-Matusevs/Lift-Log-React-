332b9427fc11bad970149acb1693ed6310f8d249
Artemijs Matusev <matu6ev@gmail.com>
2025-10-06 16:33:15 +0100

    Created histroy and workout-templates componenets

---
+++ src/components/History.jsx
@@ -0,0 +1,7 @@
+export default function History(){
+    return(
+        <div className="page">
+            <h1 className="auth-header">History</h1>
+        </div>
+    )
+}
---
+++ src/components/Templates.jsx
@@ -0,0 +1,7 @@
+export default function Templates(){
+    return(
+        <div className="page">
+            <h1 className="auth-header">Templates</h1>
+        </div>
+    )
+}
