9b2dbb65aaebf421083058f42ec671cdfa74dabe
Artemijs Matusev <matu6ev@gmail.com>
2025-10-06 16:29:47 +0100

    Linekd the nav componenets to their routes and styled active/non active componenets

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,34 @@
+import {Link, useLocation} from "react-router-dom";
+
 export default function Nav(){
+    const location = useLocation();
+
     return(
         <nav className="nav-bar">
             <ul>
-                <li> <ion-icon name="home-outline"></ion-icon> </li>
-                <li> <ion-icon name="time-outline"></ion-icon> </li>
-                <li> <ion-icon name="barbell-outline"></ion-icon> </li>
-                <li> <ion-icon name="log-out-outline"></ion-icon> </li>
+                <li> 
+                    <Link to="/dashboard" className={location.pathname === '/dashboard' ? 'active' : 'not-active'}>
+                        <ion-icon name="home-outline"></ion-icon> 
+                    </Link>
+                </li>
+
+                <li> 
+                    <Link to="/history" className={location.pathname === '/history' ? 'active' : 'not-active'}>
+                        <ion-icon name="time-outline"></ion-icon>
+                    </Link>
+                </li>
+
+                <li> 
+                    <Link to="/workout-templates" className={location.pathname === '/workout-templates' ? 'active' : 'not-active'}>
+                        <ion-icon name="barbell-outline"></ion-icon>
+                    </Link>
+                </li>
+
+                <li> 
+                    <Link to="/logout" className={location.pathname === '/logout' ? 'active' : 'not-active'}>
+                        <ion-icon name="log-out-outline"></ion-icon>
+                    </Link>
+                </li>
             </ul>
         </nav>
     )
