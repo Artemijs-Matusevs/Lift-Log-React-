d74b766bc111f982e3cc6a4608337fcf07b06b00
Artemijs Matusev <matu6ev@gmail.com>
2025-10-06 15:55:21 +0100

    Created a Nav componenet, set to render only when isAuthenticated useState is True

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+export default function Nav(){
+    return(
+        <nav className="nav-bar">
+            <ul>
+                <li> Home </li>
+                <li> History </li>
+                <li> Workout </li>
+                <li> Logout </li>
+            </ul>
+        </nav>
+    )
+}
